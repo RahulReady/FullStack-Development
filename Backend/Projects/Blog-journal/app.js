1f41eb2771744ba60f3653d188c1761f031afc54
RahulReady <52625075+RahulReady@users.noreply.github.com>
2020-06-28 22:18:42 -0500

    finish blogwebsite

--- Backend/Projects/Blog-journal/app.js
+++ Backend/Projects/Blog-journal/app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const lodash = require('lodash');
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -18,7 +18,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 let posts = [];
-let ai = [];
 
 app.get('/', function(req, res){
 
@@ -49,13 +48,30 @@ app.post('/compose', function(req, res){
     body: req.body.textBody
   };
   posts.push(post);
-
   res.redirect('/');
 
 });
 
 
+app.get('/posts/:postDay', function(req, res){
+  const requestedTitle = lodash.lowerCase(req.params.postDay);
+// for each loop
+  posts.forEach(function(post){
+    const title = lodash.lowerCase(post.title);
+    if(requestedTitle === title){
+      const link = '/posts/' + title
+      console.log('Match found!');
+      res.render('post', {post: post, link:link});
+    }
+  })
+ // For loop
+  // for(let i = 0; i < posts.length; i++){
+  //   if(posts[i]['title'] === requestedTitle){
+  //     console.log('Match found');
+  //   }
+  // }
 
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
